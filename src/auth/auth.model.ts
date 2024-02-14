97a9d8b810d765872848c889fee229eca60db77c
Platov Sergei <serg1764@gmail.com>
2024-02-14 11:28:09 +0300

    Урок 34 время 5:30

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+//import { Document } from 'mongoose';
+/** Спросить у Серёги есть ди в mongoose поля data и id и можно
+ * ли их приэкстендить например одно в клас другое через
+ * интерфейс если они в разном лежат*/
+
+@Schema()
 export class AuthModel {
+  @Prop()
+  _id: string;
+
+  @Prop({ unique: true }) // Устанавливаем уникальность для email
   email: string;
+
+  @Prop()
   passwordHash: string;
+
+  @Prop()
+  createdAt?: Date;
+
+  @Prop()
+  updatedAt?: Date;
 }
+
+export const AuthSchema = SchemaFactory.createForClass(AuthModel);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthModel, AuthSchema } from './auth.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: AuthModel.name,
+        schema: AuthSchema,
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
